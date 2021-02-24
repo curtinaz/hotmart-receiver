8a82a7b8fd2e27d48478348e597ca863755a3a13
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-02-24 11:03:33 -0300

    test

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+ 
+app.use(cors());
+app.use(express.json());
+app.listen(3333);
+
+app.send("hello world")
+
 console.log ("system running")
