e4cd35d349c8ef15feb30f7d75529f5518f493d0
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-02-24 14:56:27 -0300

    Update UsuarioController.js

--- src/controllers/UsuarioController.js
+++ src/controllers/UsuarioController.js
@@ -1,4 +1,14 @@
- exports.post = function (req, res, next) {
+ exports.post = function (req, res,) {
+
+   const keys = Object.keys(req.body)
+
+   for (key of keys) {
+       if (req.body[key] == "") {
+       return res.send ("Por favor, preencha todos os campos!")
+       }
+   }
+   
+   let { avatar_url, name, birth, gender, services } = req.body
 
    res.status(201).send('Rota POST!');
 
