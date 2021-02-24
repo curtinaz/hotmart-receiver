95c2bbaaada99ec3ac2220c150bc8a98ae5c009f
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-02-24 14:54:56 -0300

    Update UsuarioController.js

--- src/controllers/UsuarioController.js
+++ src/controllers/UsuarioController.js
@@ -1,28 +1,4 @@
- exports.post = function (req, res) {
-
-   const keys = Object.keys(req.body)
-
-   for (key of keys) {
-       if (req.body[key] == "") {
-       return res.send ("Por favor, preencha todos os campos!")
-       }
-   }
-   
-   let { avatar_url, name, birth, gender, services } = req.body
-
-   birth = Date.parse(req.body.birth)
-   const created_at = Date.now()
-   const id = Number(data.instructors.length + 1)
-
-   data.instructors.push({
-       id,
-       avatar_url,
-       name,
-       gender,
-       services,
-       birth,
-       created_at
-   }) // [{...},{...}]
+ exports.post = function (req, res, next) {
 
    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
    if (err) return res.send("Write File error!")
