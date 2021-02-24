a8bef8a6b1f733245ea435c68f47f1a02f1d9aaf
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-02-24 14:57:34 -0300

    Update UsuarioController.js

--- src/controllers/UsuarioController.js
+++ src/controllers/UsuarioController.js
@@ -1,14 +1,26 @@
  exports.post = function (req, res,) {
-
-   const keys = Object.keys(req.body)
-
-   for (key of keys) {
-       if (req.body[key] == "") {
-       return res.send ("Por favor, preencha todos os campos!")
-       }
-   }
    
    let { avatar_url, name, birth, gender, services } = req.body
+
+   birth = Date.parse(req.body.birth)
+   const created_at = Date.now()
+   const id = Number(data.instructors.length + 1)
+
+   data.instructors.push({
+       id,
+       avatar_url,
+       name,
+       gender,
+       services,
+       birth,
+       created_at
+   }) // [{...},{...}]
+
+   fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+   if (err) return res.send("Write File error!")
+
+   return res.redirect(`/instructors/${id}`)
+   })
 
    res.status(201).send('Rota POST!');
 
