b27dd9246c6657c08171b181ccb382017e96f7c4
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-02-24 11:09:31 -0300

    changes

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
- 
+
+require('./src/Routes/index')(app); // <--- basta adicionar essa linha
+
 app.use(cors());
 app.use(express.json());
 app.listen(3333);
+
+
 
 console.log ("system running")
